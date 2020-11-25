b0e542dbc03b76caba3dd6674a7658efb48e2ce5
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2020-11-25 19:42:55 +0530

    Scrap company name and skills for page 1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 This works for Jobs only posted few days back having python as one of the requirement.
-It will scrap the jobs only on the first page of the website
+It will scrap the jobs only on the first page of the website.
 
 '''
 
@@ -11,5 +11,12 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
                          '=submit&txtKeywords=python&txtLocation=')
 
 soup = BeautifulSoup(html_text.text, 'lxml')
+
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-print(jobs)
+for job in jobs:
+    published_date = job.find('span', class_='sim-posted').span.text
+    if 'few' in published_date:
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+        print(f"Company name : {company_name.strip()}\nRequired skills : {skills.strip()}")
+        print()
