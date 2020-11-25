a22af07b55135f0716a7c7b2e61c8c6d275d15ca
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2020-11-25 18:48:26 +0530

    Basic scrapping

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-from bs4 import BeautifulSoup
+'''
+This works for Jobs only posted few days back having python as one of the requirement.
+It will scrap the jobs only on the first page of the website
 
+'''
+
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from'
+                         '=submit&txtKeywords=python&txtLocation=')
+
+soup = BeautifulSoup(html_text.text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+print(jobs)
