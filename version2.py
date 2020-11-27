9af56390c38e58a49bcd432cbf46e84bd3defcee
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2020-11-27 12:50:00 +0530

    Dynamic searching for both

--- version2.py
+++ version2.py
@@ -8,21 +8,24 @@ import requests
 import time
 
 
-def techgig_find_jobs():
+def index():
     skills = input("Looking for a job in the field? (python, java, etc) : ").split()
-    location = input("Location ? : ")
+    location = input("Location ? : ").lower()
     date_posted = int(input("How recent? 1 - for 1 week, 2 - 2 week, 0 - None : "))
-    experience = input("Previous experience : 0 - fresher, 1 for 1 year and so on : ")
+    experience = int(input("Previous experience : 0 - fresher, 1 for 1 year and so on : "))
 
-    print(f"Skills : {skills}\nlocation : {location}\nPosted on : {date_posted}\nExperience : {experience}\n")
+    techgig_find_jobs(skills, location, date_posted, experience)
+    times_find_jobs(skills, location, date_posted, experience)
+
+
+def techgig_find_jobs(skills, location, date_posted, experience):
 
     url = f"https://www.techgig.com/job_search?page=1&txtKeyword={'+'.join(skills)}&cboWorkExp1={experience}&" \
           f"NoOfDaysSincePosted={date_posted * 7}&keyword=&txtLocation={location}"
-    
-    print("URL : "+url)
+
+    # print("URL : "+url)
     print("-----TECHGIG-----")
-    # skill_unfamiliar = input(f"Enter skill you are not familiar with : ")
-    # print(f"Filtering job not having skill : {skill_unfamiliar}")
+
     html_text = requests.get(url)
     soup = BeautifulSoup(html_text.text, 'lxml')
 
@@ -46,12 +49,12 @@ def techgig_find_jobs():
         print("\n")
 
 
-def times_find_jobs():
+def times_find_jobs(skills, location, date_posted, experience):
     print("-----Timesjobs-----")
-    skill_unfamiliar = input("Enter skill you are not familiar with : ")
-    print(f"Filtering jobs not having skill : {skill_unfamiliar}")
-    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from'
-                             '=submit&txtKeywords=python&txtLocation=')
+
+    url = f"https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&f" \
+          f"rom=submit&txtKeywords={'+'.join(skills)}&txtLocation={location}&cboWorkExp1={experience}"
+    html_text = requests.get(url)
 
     soup = BeautifulSoup(html_text.text, 'lxml')
 
@@ -59,19 +62,13 @@ def times_find_jobs():
     # print(jobs)
     for index, job in enumerate(jobs):  # index for literally the index of the job
         published_date = job.find('span', class_='sim-posted').span.text
-        if 'few' in published_date:
-            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
-            more_info = job.header.h2.a['href']
+        title = job.find('header', class_='clearfix').h2.a.text
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+        more_info = job.header.h2.a['href']
 
-            if skill_unfamiliar not in skills:
-                '''with open(f'posts/job_{index}.txt', 'w') as f:
-                    f.write(f"Company name : {company_name.strip()}\nRequired skills : {skills.strip()}\n")
-                    f.write(f"More info : {more_info}")
-                print(f"File {index} saved!")'''  # To save data in text file
-
-                print(f"Company name : {company_name.strip()}\nRequired skills : {skills.strip()}")
-                print(f"More info : {more_info}\n")
+        print(f"Job Title : {title.strip()}\nCompany name : {company_name.strip()}\nRequired skills : {skills.strip()}")
+        print(f"Publihsed on : {published_date}\nMore info : {more_info}\n")
 
 
 if __name__ == '__main__':
@@ -80,5 +77,6 @@ if __name__ == '__main__':
         time_wait = 10
         print(f"Waiting {time_wait} minutes...")
         time.sleep(time_wait * 60)'''
-    techgig_find_jobs()
+    # techgig_find_jobs()
     # times_find_jobs()
+    index()
